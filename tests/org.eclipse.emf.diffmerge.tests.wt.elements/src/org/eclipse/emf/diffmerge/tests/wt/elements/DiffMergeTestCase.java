ca9f2b1b1841fac208abb73556c987e016f4da24
Olivier Constant <olivier.constant@thalesgroup.com>
2014-06-04 18:22:42 +0200

    Updated the editor header for consistency with Eclipse Compare.
    Change-Id: I468c07bf7de02a9b2192f3ff414f124d19575a52

--- tests/org.eclipse.emf.diffmerge.tests.wt.elements/src/org/eclipse/emf/diffmerge/tests/wt/elements/DiffMergeTestCase.java
+++ tests/org.eclipse.emf.diffmerge.tests.wt.elements/src/org/eclipse/emf/diffmerge/tests/wt/elements/DiffMergeTestCase.java
@@ -39,7 +39,7 @@ public abstract class DiffMergeTestCase extends UITestCaseSWT {
   private static IProject __predefinedProject, __currentProject;
   private static final String MODEL1 = "Ex1.elements";
   private static final String MODEL2 = "Ex2.elements";
-  private static final String EDITOR_TITLE = "Compare [Ex1 - Ex2]";
+  private static final String EDITOR_TITLE = "Compare ('Ex1' - 'Ex2')";
   private static final char SEP = '/';
   
   
