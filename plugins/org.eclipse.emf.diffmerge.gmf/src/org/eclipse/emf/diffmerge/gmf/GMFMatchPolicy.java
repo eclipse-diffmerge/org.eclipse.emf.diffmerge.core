00dd40a0cc845b3bb687b8a5f3da53d767316b09
Olivier Constant <olivier.constant@thalesgroup.com>
2014-08-19 11:48:11 +0200

    Bug 442026 - Incorrect copyright in certain classes
    Change-Id: Idbeabdf2c45baddae03cd84c0e93dde73aa5f13b
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- plugins/org.eclipse.emf.diffmerge.gmf/src/org/eclipse/emf/diffmerge/gmf/GMFMatchPolicy.java
+++ plugins/org.eclipse.emf.diffmerge.gmf/src/org/eclipse/emf/diffmerge/gmf/GMFMatchPolicy.java
@@ -1,5 +1,16 @@
 /**
- * Copyright (c) THALES, 2013. All rights reserved.
+ * <copyright>
+ * 
+ * Copyright (c) 2010-2014 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thales Global Services S.A.S. - initial API and implementation
+ * 
+ * </copyright>
  */
 package org.eclipse.emf.diffmerge.gmf;
 
