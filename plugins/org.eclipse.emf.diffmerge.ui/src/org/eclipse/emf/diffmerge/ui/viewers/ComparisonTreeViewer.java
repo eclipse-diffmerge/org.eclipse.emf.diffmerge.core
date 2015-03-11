88737fac910bde6b0aec256af6860afa8a0a9626
Olivier Constant <olivier.constant@thalesgroup.com>
2015-03-11 17:04:37 +0100

    Bug 461936 - NPEs in GUI when diff policy ignores matches
    Change-Id: I1b43980ba70c86ceea9ebefd9603a6a3c6b7799e
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- plugins/org.eclipse.emf.diffmerge.ui/src/org/eclipse/emf/diffmerge/ui/viewers/ComparisonTreeViewer.java
+++ plugins/org.eclipse.emf.diffmerge.ui/src/org/eclipse/emf/diffmerge/ui/viewers/ComparisonTreeViewer.java
@@ -196,7 +196,8 @@ public class ComparisonTreeViewer extends TreeViewer {
   }
   
   /**
-   * Return a list of lists representing the possible paths to the given match
+   * Return a list of lists representing the possible paths to the given match.
+   * The current input is assumed to be non-null.
    * @param match_p a potentially null match
    * @param parentsOnly_p whether the given match must be excluded from the paths
    * @return a non-null list
@@ -206,7 +207,7 @@ public class ComparisonTreeViewer extends TreeViewer {
     if (match_p == null) {
       result = new ArrayList<List<IMatch>>();
     } else {
-      IComparison comparison = match_p.getMapping().getComparison();
+      IComparison comparison = getInput().getActualComparison();
       Role drivingRole = getDrivingRole();
       boolean isRoot = comparison.getContents(drivingRole).contains(match_p) ||
         match_p.getUncoveredRole() == drivingRole &&
