172f4aade62d4f6d0cf3b9c1ce0b3cbc50ecc43e
Olivier Constant <olivier.constant@thalesgroup.com>
2019-09-24 15:20:51 +0200

    [releng] Update of copyrights
    Change-Id: I5046d9d9427185c7a0203a8dc32861b873f88c43
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- plugins/org.eclipse.emf.diffmerge.ui/src/org/eclipse/emf/diffmerge/ui/util/SymmetricMatchComparer.java
+++ plugins/org.eclipse.emf.diffmerge.ui/src/org/eclipse/emf/diffmerge/ui/util/SymmetricMatchComparer.java
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2018 Thales Global Services S.A.S and others.
+ * Copyright (c) 2018-2019 Thales Global Services S.A.S and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
